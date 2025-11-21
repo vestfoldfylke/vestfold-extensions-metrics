983bbb4f664efdaf8f009cc5ce078a163a8e1db0
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-11-21 08:55:54 +0100

    chore: Turn off ImplicitUsings

--- Vestfold.Extensions.Metrics.Test/MetricsServiceTest.cs
+++ Vestfold.Extensions.Metrics.Test/MetricsServiceTest.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Vestfold.Extensions.Metrics.Services;
 
 namespace Vestfold.Extensions.Metrics.Test;
--- Vestfold.Extensions.Metrics/Services/MetricsService.cs
+++ Vestfold.Extensions.Metrics/Services/MetricsService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Linq;
 using Prometheus;
 using ITimer = Prometheus.ITimer;
 
